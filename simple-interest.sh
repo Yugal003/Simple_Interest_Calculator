038d425921fd69c21c1c969b7d8c1686bed842b8
Yugal003 <121950831+Yugal003@users.noreply.github.com>
2024-09-30 16:02:43 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to calculate simple interest
+echo "Enter principal:"
+read principal
+echo "Enter rate of interest:"
+read rate
+echo "Enter time (in years):"
+read time
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+echo "The Simple Interest is: $simple_interest"
